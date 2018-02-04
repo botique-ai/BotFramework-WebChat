97af990cd59e50ad85a39594ba26d3a5ffe555bc
Pavel Tarno <paveltarno@gmail.com>
2018-02-04 15:55:38 +0200

    updated component to export HistoryView and defaultStrings

--- src/BotChat.ts
+++ src/BotChat.ts
@@ -1,9 +1,11 @@
 export { App, AppProps } from './App';
 export { Chat, ChatProps } from './Chat';
+export { HistoryView } from './History';
 export * from '@botique/botframework-directlinejs';
 export { queryParams } from './Attachment';
 export { SpeechOptions } from './SpeechOptions'
 export { Speech } from './SpeechModule'
+export { defaultStrings } from './Strings';
 import { FormatOptions } from './Types';
 // below are shims for compatibility with old browsers (IE 10 being the main culprit)
 import 'core-js/modules/es6.string.starts-with';
